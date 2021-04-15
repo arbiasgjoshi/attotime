f45f914ce07b0d54d9d1cacbeb6b9ee3d776b00e
Enis <enisk@starlabspro.com>
2021-04-16 01:27:50 +0200

    Replaced margins with divider components.

--- src/components/molecules/image-list/index.js
+++ src/components/molecules/image-list/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { imageListContainer } from './image-list.module.scss';
 import { StaticImage } from 'gatsby-plugin-image';
 
-const ImageList = () => {
-  return (
-    <div className={imageListContainer}>
-      <StaticImage
-        alt="Clock In"
-        width={546}
-        height={474}
-        src={'../../../images/time-tracking-clock.svg'}
-      />
-    </div>
-  );
-};
+const ImageList = () => (
+  <div>
+    <StaticImage
+      alt="Clock In"
+      width={546}
+      height={474}
+      src={'../../../images/time-tracking-clock.svg'}
+    />
+  </div>
+);
 
 export default ImageList;
