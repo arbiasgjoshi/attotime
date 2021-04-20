c08936360ec58325d60376569c1f8ea9fd4eefd7
Enis <enisk@starlabspro.com>
2021-04-20 23:14:45 +0200

    Added some parts on pricing, only one component left

--- src/pages/pricing/index.js
+++ src/pages/pricing/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import HeaderComponent from '@components/molecules/header';
 import SEO from '@components/molecules/seo';
 import { FooterLinks } from '@locale/en.js';
+import Button from '@components/atoms/button';
 import Divider from '@components/atoms/divider';
 import SubscribeBanner from '@components/molecules/subscribe-banner';
 import FooterComponent from '@components/molecules/footer';
@@ -12,9 +13,29 @@ import Story from '@components/organisms/story';
 import FeaturesList from '@components/organisms/features-list';
 
 import { container } from '@styles/main.module.scss';
-import { packageWrapper } from './pricing.module.scss';
+import {
+  packageWrapper,
+  priceHeader,
+  buttonContainer,
+  rightSide,
+  featuresBanner,
+  questionsContainer,
+  list,
+  item,
+  plus,
+} from './pricing.module.scss';
 
 import authorImage2 from '@images/authorImage2.png';
+import { StaticImage } from 'gatsby-plugin-image';
+import icon1 from '@images/timer.png';
+import icon2 from '@images/location@2x.png';
+import icon3 from '@images/profile@2x.png';
+import icon4 from '@images/timesheets@2x.png';
+import icon5 from '@images/wage_estimates@2x.png';
+import icon6 from '@images/job_costing@2x.png';
+import icon7 from '@images/mobile_time_tracking@2x.png';
+import icon8 from '@images/bookmark.png';
+import icon9 from '@images/help_support@2x.png';
 
 const Pricing = () => {
   const checkList = [
@@ -46,46 +67,55 @@ const Pricing = () => {
       description:
         'Enjoy highly accurate, easy-to-use, and trustworthy time tracking, anytime and from anywhere.',
       alt: 'Clock logo',
+      logo: icon1,
     },
     {
       title: 'GPS Location Tracking',
       description: `Increase the accountability and transparency of your team with real-time updates on their location`,
       alt: 'GPS icon',
+      logo: icon2,
     },
     {
       title: 'Team Activity Insights',
       description: `See where your team is, and what they're working on - without back and forth phone calls.`,
       alt: 'profile pic',
+      logo: icon3,
     },
     {
       title: 'Automated Timesheets',
       description: `Spend more time on the things that matter with timesheets filled painlessly and on-time.`,
       alt: '',
+      logo: icon4,
     },
     {
       title: 'Simplified Payroll',
       description: `Save hours from the payroll process with built-in integrations and automated email reports.`,
       alt: '',
+      logo: icon5,
     },
     {
       title: 'Professional Reports',
       description: `Download and share insightful custom reports into your team's activity on the clock.`,
       alt: '',
+      logo: icon6,
     },
     {
       title: 'Simple Time Clock',
       description: `Track time wherever your team is. In the office, at a job site, or on the go through Mobile, Web, and Kiosk Apps.`,
       alt: '',
+      logo: icon7,
     },
     {
       title: 'Jobs, Breaks, Notes',
       description: `Get more data on what your employees are up to with job codes, break hours, insightful notes, and more.`,
       alt: '',
+      logo: icon8,
     },
     {
       title: 'Unlimited Support',
       description: `Get free unlimited help from our support team whenever you need it.`,
       alt: '',
+      logo: icon9,
     },
   ];
 
@@ -93,6 +123,16 @@ const Pricing = () => {
     <div className={container}>
       <SEO title="Pricing" />
       <HeaderComponent />
+      <div className={priceHeader}>
+        <h2>Pay-as-you-grow pricing</h2>
+        <div className={rightSide}>
+          <div className={buttonContainer}>
+            <Button btnText="Monthly" btnStyle="round" />
+            <Button btnText="Annually" btnStyle="round" />
+          </div>
+          <p>Save 29%</p>
+        </div>
+      </div>
       <Divider />
       <div className={packageWrapper}>
         <PackageCard
@@ -118,6 +158,10 @@ const Pricing = () => {
         title="That's not all!"
         description={`30+ more features included in our Premium & Enterprise Plans`}
       />
+      <div className={featuresBanner}>
+        <StaticImage src="../../images/video-background.svg" width={1140} height={328} />
+        <Button btnText="View All Features" btnStyle="wide" />
+      </div>
       <Divider className="medium" />
       <Story
         img={authorImage2}
@@ -126,9 +170,41 @@ const Pricing = () => {
       />
       <Divider />
       <Title title="All our main features, included in one simple plan" />
-      <Divider className="medium" />
       <FeaturesList list={featureList} />
-      <Divider className="medium" />
+      <Divider />
+      <StaticImage src="../../images/field_workers@2x.png" width={1140} height={460} />
+      <Divider />
+      <div className={questionsContainer}>
+        <Title title="Frequently asked questions" />
+        <div className={list}>
+          <div className={item}>
+            <h5>
+              What happens at the end of my free trial? <span className={plus}>-</span>
+            </h5>
+            <p>
+              After your 14-day free trial ends, you'll be asked to purchase a subscription in order
+              to continue using Atto. As we don't require a credit card to sign up for our free
+              trial, you won't be charged unless you actively choose to subscribe.
+            </p>
+          </div>
+          <div className={item}>
+            <h5>
+              Are there any usage limits or add-on charges ? <span className={plus}>+</span>
+            </h5>
+          </div>
+          <div className={item}>
+            <h5>
+              What payment options do you accept? <span className={plus}>+</span>
+            </h5>
+          </div>
+          <div className={item}>
+            <h5>
+              I have a question that isn't answered <span className={plus}>+</span>
+            </h5>
+          </div>
+        </div>
+        <Divider />
+      </div>
       <SubscribeBanner
         bannerImage
         title="There's no time to waste!"
