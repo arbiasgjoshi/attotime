edc79d762677701b0e59bb852b7a5e862d1e3568
arbiasgjoshi <arbiasgjoshi@gmail.com>
2021-05-02 23:42:11 +0200

    menu styling and fixing hover states

--- src/components/molecules/header/index.js
+++ src/components/molecules/header/index.js
@@ -21,39 +21,23 @@ import {
   featuredItems,
   featureLink,
   headerItem,
+  menuItem,
   menuItemWrapper,
+  resourcesMenu,
   menuIcon,
   menuItemDesc,
 } from './header.module.scss';
 
 const HeaderComponent = ({ centered, logoType }) => {
-  const [hovered, setHovered] = useState(null);
+  const [hovered, setHovered] = useState(false);
+  const [activeItem, setActiveItem] = useState(null);
+  const [aboveMainMenu, setAbove] = useState(false);
+  const [overMenu, setOverMenu] = useState(false);
 
-  const onElementHover = (val) => {
-    setHovered(val);
-  };
-
-  return (
-    <>
-      <header className={headerContainer}>
-        <div className={leftNav}>
-          <StaticImage src="../../../images/logo@2x.png" quality={90} width={85} />
-        </div>
-        <div className={menuLinks}>
-          <Link to="/product">Product</Link>
-          <Link to="/why-atto">Why Atto?</Link>
-          <Link to="/industries">Industries</Link>
-          <Link to="/pricing">Pricing</Link>
-          <Link to="/resources">Resources</Link>
-        </div>
-        <div className={rightNavButtons}>
-          <Link to="/login">Log In</Link>
-          <Link to="/try-for-free">
-            <Button btnText="Try for Free" btnStyle="teal" />
-          </Link>
-        </div>
-
-        <div className={menu}>
+  const viewMenuSections = (val) => {
+    switch (val) {
+      case 'product':
+        return (
           <div className={menuSection}>
             <div className={`${leftItems} ${contentPadding} `}>
               <div className={mainItem}>
@@ -71,7 +55,44 @@ const HeaderComponent = ({ centered, logoType }) => {
                 </Link>
               </div>
               <Divider className="style5" />
-              <div className={subItems}></div>
+              <div className={subItems}>
+                <Link className={menuItem} to="/product/time-tracking">
+                  <div className={menuIcon}>
+                    <Icon iconClass="random" />
+                  </div>
+                  <div className={menuItemDesc}>
+                    <h6>Time Tracking</h6>
+                    <p>Accurate time tracking from any location and any device</p>
+                  </div>
+                </Link>
+                <Link className={menuItem} to="/product/gps-location-tracking">
+                  <div className={menuIcon}>
+                    <Icon iconClass="random" />
+                  </div>
+                  <div className={menuItemDesc}>
+                    <h6>GPS Location Tracking</h6>
+                    <p>Real-time updates on your team’s locations and movements</p>
+                  </div>
+                </Link>
+                <Link className={menuItem} to="/">
+                  <div className={menuIcon}>
+                    <Icon iconClass="random" />
+                  </div>
+                  <div className={menuItemDesc}>
+                    <h6>Team Activity</h6>
+                    <p>Stay in the loop of your team’s progress as work happens</p>
+                  </div>
+                </Link>
+                <Link className={menuItem} to="/">
+                  <div className={menuIcon}>
+                    <Icon iconClass="random" />
+                  </div>
+                  <div className={menuItemDesc}>
+                    <h6>Timesheets</h6>
+                    <p>Accurate timesheet reports without the hassle</p>
+                  </div>
+                </Link>
+              </div>
             </div>
             <div className={rightItems}>
               <h6>More Features</h6>
@@ -109,9 +130,91 @@ const HeaderComponent = ({ centered, logoType }) => {
               </ul>
             </div>
           </div>
-          <div className={`${menuSection} ${contentPadding}`} />
-          <div className={`${menuSection} ${contentPadding}`} />
+        );
+      case 'industries':
+        return <div className={`${menuSection} ${contentPadding}`} />;
+      case 'resources':
+        return <div className={`${menuSection} ${contentPadding}`} />;
+      default:
+        return null;
+    }
+  };
+
+  const leaveMenuItem = () => {
+    if (!overMenu) {
+      console.log('entering first here');
+      setTimeout(() => {
+        console.log('entering then here');
+        setHovered(false);
+        setActiveItem(null);
+      }, 500);
+    }
+  };
+
+  const hasLeftMenu = () => {
+    console.log('left menu');
+    setTimeout(() => {
+      setOverMenu(false);
+      leaveMenuItem();
+    }, 500);
+  };
+
+  // const aboveMainMenu = () => {
+
+  // }
+
+  const menuClasses = (val) => {
+    let menuClass = menu;
+    if (val === 'resources') {
+      menuClass += ` ${resourcesMenu}`;
+    }
+
+    return menuClass;
+  };
+
+  const showMenu = (val) => {
+    setHovered(true);
+    setActiveItem(val);
+  };
+
+  return (
+    <>
+      <header className={headerContainer}>
+        <div className={leftNav}>
+          <StaticImage src="../../../images/logo@2x.png" quality={90} width={85} />
         </div>
+        <div
+          className={menuLinks}
+          onMouseEnter={() => setAbove(true)}
+          onMouseLeave={() => leaveMenuItem()}
+        >
+          <Link to="/product" onMouseEnter={() => showMenu('product')}>
+            Product
+          </Link>
+          <Link to="/why-atto">Why Atto?</Link>
+          <Link to="/industries" onMouseEnter={() => showMenu('industries')}>
+            Industries
+          </Link>
+          <Link to="/pricing">Pricing</Link>
+          <Link to="/resources" onMouseEnter={() => showMenu('resources')}>
+            Resources
+          </Link>
+        </div>
+        <div className={rightNavButtons}>
+          <Link to="/login">Log In</Link>
+          <Link to="/try-for-free">
+            <Button btnText="Try for Free" btnStyle="teal" />
+          </Link>
+        </div>
+        {hovered && (
+          <div
+            className={menuClasses(activeItem)}
+            onMouseEnter={() => setOverMenu(true)}
+            onMouseLeave={() => hasLeftMenu()}
+          >
+            {viewMenuSections(activeItem)}
+          </div>
+        )}
       </header>
     </>
   );
