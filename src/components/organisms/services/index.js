8232ded0f726fe5f6f06d731a3782a0f82b96e6a
Enis <enisk@starlabspro.com>
2021-05-04 23:16:43 +0200

    Added arrow image and fixed services middle card

--- src/components/organisms/services/index.js
+++ src/components/organisms/services/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ServiceCard from '@components/molecules/service-card';
-import { container, serviceList, middleCard } from './services.module.scss';
+import { container, serviceList, middleCard, arrow, iconContainer } from './services.module.scss';
 import { StaticImage } from 'gatsby-plugin-image';
+import sparkIcon from '@images/spark@2x.png';
 
 const Services = ({ list }) => (
   <div className={container}>
@@ -10,7 +11,11 @@ const Services = ({ list }) => (
         <ServiceCard key={index} title={service.title} icon={service.icon} />
       ))}
       <div className={middleCard}>
+        <div className={iconContainer}>
+          <img height={60} width={60} src={sparkIcon} alt="Spark Icon" />
+        </div>
         <StaticImage width={364} height={124} src="../../../images/no-image.png" />
+        <div className={arrow}></div>
       </div>
     </div>
   </div>
