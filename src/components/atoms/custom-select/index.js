7748d2e69c95144041d27cb40801cef53d195e64
arbiasgjoshi <arbiasgjoshi@gmail.com>
2021-05-08 00:51:38 +0200

    custom select dropdown changes

--- src/components/atoms/custom-select/index.js
+++ src/components/atoms/custom-select/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import { select } from './custom-select.module.scss';
+import React, { useState } from 'react';
+import {
+  selectDropdown,
+  invertedArrow,
+  selectedItem,
+  optionsDropdown,
+  activeItem,
+  isOpened,
+  option,
+  backdropContainer,
+} from './custom-select.module.scss';
 import ArrowDown from '../../../images/arrow-down.svg';
 
 const CustomSelect = () => {
+  const [selected, setSelected] = useState('English');
+  const [open, setOpen] = useState(false);
+
+  const languages = [
+    {
+      id: 'en',
+      name: 'English',
+    },
+    {
+      id: 'es',
+      name: 'Español',
+    },
+    {
+      id: 'fr',
+      name: 'Française',
+    },
+    {
+      id: 'de',
+      name: 'Deutsch',
+    },
+  ];
+
+  const buttonClass = (item) => {
+    let defaultClass = option;
+    if (item.name === selected) {
+      defaultClass += ` ${activeItem}`;
+    }
+    return defaultClass;
+  };
+
+  const dropDownState = () => {
+    let dropdownClasses = optionsDropdown;
+    if (open) {
+      dropdownClasses += ` ${isOpened}`;
+    }
+    return dropdownClasses;
+  };
+
+  const toggleDropdown = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div className={select}>
-      <select>
-        <option>English</option>
-      </select>
-      <span>
-        <span>English</span>
-        {<ArrowDown />}
-      </span>
+    <div
+      className={selectDropdown}
+      onKeyDown={() => toggleDropdown()}
+      role="menu"
+      tabIndex="0"
+      onClick={() => toggleDropdown()}
+    >
+      <div className={selectedItem}>
+        <span>{selected}</span>
+        <ArrowDown className={open ? invertedArrow : null} />
+      </div>
+      <div className={dropDownState}>
+        {languages.map((lang) => (
+          <button
+            className={buttonClass(lang)}
+            onClick={() => setSelected(lang.name)}
+            type="button"
+          >
+            {lang.name}
+          </button>
+        ))}
+      </div>
+      <div
+        className={backdropContainer}
+        onKeyDown={() => {}}
+        role="menu"
+        tabIndex="-1"
+        onClick={() => setOpen(false)}
+      />
     </div>
   );
 };
