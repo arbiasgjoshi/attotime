93b9ac95aa12ce231720df81055f9de647ed8a24
Enis <enisk@starlabspro.com>
2021-05-15 17:40:49 +0200

    Fixed free trial at homepage

--- src/components/organisms/free-trial/index.js
+++ src/components/organisms/free-trial/index.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CheckCard from '@components/molecules/check-card';
-import { container, firstBox, listWrapper, ticksWrapper, tickItem } from './free-trial.module.scss';
+
 import Input from '@components/atoms/input';
 import Button from '@components/atoms/button';
 import Icon from '@components/atoms/icon';
+import CheckCard from '@components/molecules/check-card';
 
-const FreeTrial = ({ title, description, list = [] }) => {
-  return (
-    <div className={container}>
-      <div className={firstBox}>
-        <h3>{title}</h3>
-        <h4>{description}</h4>
-        <Input placeholder={'Type your email'} />
-        <Button btnText="Start a Free Trial - 14 Days Free" btnStyle="black" />
-        <div className={ticksWrapper}>
-          <div className={tickItem}>
-            <Icon iconClass="tick" />
-            <span>No credit card required</span>
-          </div>
-          <div className={tickItem}>
-            <Icon iconClass="tick" />
-            <span>Cancel anytime</span>
-          </div>
+import { container, firstBox, listWrapper, ticksWrapper, tickItem } from './free-trial.module.scss';
+
+const FreeTrial = ({ title, description, list = [] }) => (
+  <div className={container}>
+    <div className={firstBox}>
+      <h3>{title}</h3>
+      <h4>{description}</h4>
+      <Input placeholder="Type your email" />
+      <Button btnText="Start a Free Trial - 14 Days Free" btnStyle="black" />
+      <div className={ticksWrapper}>
+        <div className={tickItem}>
+          <Icon iconClass="tick" />
+          <span>No credit card required</span>
+        </div>
+        <div className={tickItem}>
+          <Icon iconClass="tick" />
+          <span>Cancel anytime</span>
         </div>
       </div>
-      <div className={listWrapper}>
-        {list.map(({ title }) => (
-          <CheckCard title={title} />
-        ))}
-      </div>
     </div>
-  );
-};
+    <div className={listWrapper}>
+      {list.map(({ title: listItemTitle }) => (
+        <CheckCard title={listItemTitle} />
+      ))}
+    </div>
+  </div>
+);
 
 FreeTrial.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
-  list: PropTypes.array,
+  list: PropTypes.arrayOf({}),
 };
 
 export default FreeTrial;
