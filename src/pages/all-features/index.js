2fa3af19975139a8639045330b0538812b97f160
Enis <enisk@starlabspro.com>
2021-05-19 04:32:27 +0200

    Fixed the build error and added a mask at all features

--- src/pages/all-features/index.js
+++ src/pages/all-features/index.js
@@ -14,7 +14,7 @@ import {
   locationTrackingCards,
   timesheetCards,
   teamActivityCards,
-} from './data';
+} from '../../dummyData/all-features';
 
 import Faq from '@components/organisms/faq';
 import FooterComponent from '@components/molecules/footer';
@@ -29,100 +29,104 @@ import {
   teamActivityStyle,
   timesheetStyle,
   cardWrapper,
+  behindMask,
 } from './all-features.module.scss';
 
 const AllFeaturesPage = () => {
   const someVar = null;
   return (
-    <div className={container}>
-      <SEO title="All features" />
-      <HeaderComponent headerStyle="pricingHeader" />
-      <MainTitleCard
-        title="All features, at a glance"
-        maxParagraphWidth={620}
-        subtitle="Combining everything you need to manage your team in one place."
-      />
-      <Divider />
-      <Divider className="style3" />
-      <Title title="Time Tracking Features" />
-      <Divider className="style5" />
-      <div className={`${cardListWrapper} ${timeTrackingStyle}`}>
-        {timeTrackingCards.map(({ title, description, logo }) => {
-          return (
-            <div className={cardWrapper}>
-              <FeatureCard
-                style="all-features"
-                title={title}
-                description={description}
-                logo={logo}
-              />
-            </div>
-          );
-        })}
+    <>
+      <span className={behindMask} />
+      <div className={container}>
+        <SEO title="All features" />
+        <HeaderComponent headerStyle="pricingHeader" />
+        <MainTitleCard
+          title="All features, at a glance"
+          maxParagraphWidth={620}
+          subtitle="Combining everything you need to manage your team in one place."
+        />
+        <Divider className="style4" />
+        <Divider className="style3" />
+        <Title title="Time Tracking Features" />
+        <Divider className="style5" />
+        <div className={`${cardListWrapper} ${timeTrackingStyle}`}>
+          {timeTrackingCards.map(({ title, description, logo }) => {
+            return (
+              <div className={cardWrapper}>
+                <FeatureCard
+                  style="all-features"
+                  title={title}
+                  description={description}
+                  logo={logo}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <Divider className="style4" />
+        <Title title="Location Tracking Features" />
+        <Divider className="style5" />
+        <div className={`${cardListWrapper} ${locationTrackingStyle}`}>
+          {locationTrackingCards.map(({ title, description, logo }) => {
+            return (
+              <div className={cardWrapper}>
+                <FeatureCard
+                  style="all-features"
+                  title={title}
+                  description={description}
+                  logo={logo}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <Divider className="style4" />
+        <Title title="Team Activity Features" />
+        <Divider className="style5" />
+        <div className={`${cardListWrapper} ${teamActivityStyle}`}>
+          {teamActivityCards.map(({ title, description, logo }) => {
+            return (
+              <div className={cardWrapper}>
+                <FeatureCard
+                  style="all-features"
+                  title={title}
+                  description={description}
+                  logo={logo}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <Divider className="style4" />
+        <Title title="Timesheets Features" />
+        <Divider className="style5" />
+        <div className={`${cardListWrapper} ${timesheetStyle}`}>
+          {timesheetCards.map(({ title, description, logo }) => {
+            return (
+              <div className={cardWrapper}>
+                <FeatureCard
+                  style="all-features"
+                  title={title}
+                  description={description}
+                  logo={logo}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <SubscribeBanner
+          bannerImage
+          title="There’s no time to waste!"
+          subtitle="Try Atto, free for 14 days"
+          placeholder="Type your email"
+          checkItemOne="No credit card required"
+          checkItemTwo="14 day free trial"
+          checkItemThree="Cancel anytime"
+          style="pricing"
+        />
+        <FooterComponent FooterLinks={FooterLinks} />
       </div>
-      <Divider className="style4" />
-      <Title title="Location Tracking Features" />
-      <Divider className="style5" />
-      <div className={`${cardListWrapper} ${locationTrackingStyle}`}>
-        {locationTrackingCards.map(({ title, description, logo }) => {
-          return (
-            <div className={cardWrapper}>
-              <FeatureCard
-                style="all-features"
-                title={title}
-                description={description}
-                logo={logo}
-              />
-            </div>
-          );
-        })}
-      </div>
-      <Divider className="style4" />
-      <Title title="Team Activity Features" />
-      <Divider className="style5" />
-      <div className={`${cardListWrapper} ${teamActivityStyle}`}>
-        {teamActivityCards.map(({ title, description, logo }) => {
-          return (
-            <div className={cardWrapper}>
-              <FeatureCard
-                style="all-features"
-                title={title}
-                description={description}
-                logo={logo}
-              />
-            </div>
-          );
-        })}
-      </div>
-      <Divider className="style4" />
-      <Title title="Timesheets Features" />
-      <Divider className="style5" />
-      <div className={`${cardListWrapper} ${timesheetStyle}`}>
-        {timesheetCards.map(({ title, description, logo }) => {
-          return (
-            <div className={cardWrapper}>
-              <FeatureCard
-                style="all-features"
-                title={title}
-                description={description}
-                logo={logo}
-              />
-            </div>
-          );
-        })}
-      </div>
-      <SubscribeBanner
-        bannerImage
-        title="There’s no time to waste!"
-        subtitle="Try Atto, free for 14 days"
-        placeholder="Type your email"
-        checkItemOne="No credit card required"
-        checkItemTwo="14 day free trial"
-        checkItemThree="Cancel anytime"
-        style="pricing"
-      />
-      <FooterComponent FooterLinks={FooterLinks} />
-    </div>
+    </>
   );
 };
 
