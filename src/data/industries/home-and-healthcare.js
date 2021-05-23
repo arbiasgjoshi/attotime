fc238423f3d23d44f7322bcc6517dfd6fe9b99ba
Enis <enisk@starlabspro.com>
2021-05-23 19:48:52 +0200

    Added component at healthcare

---
+++ src/data/industries/home-and-healthcare.js
@@ -0,0 +1,91 @@
+export const checkList1 = [
+  { title: 'Keep accurate time records while working outdoors.' },
+  { title: 'Track your roofers’ time and location in real-time.' },
+  { title: 'Respond quickly to changes in the weather.' },
+  { title: 'Improve the accuracy of quotes to win more jobs.' },
+];
+
+export const accordionList1 = [
+  {
+    icon: '',
+    title: 'Adjust quickly to employee sickness and absence ',
+    description:
+      'When caregivers are on sick leave, you need to make sure someone else can provide patient care at short notice. Atto allows you to quickly see who’s nearby and available to work – without having to call around.',
+  },
+  {
+    icon: '',
+    title: 'Prevent caregivers from clocking in until they’re ready to work',
+    description:
+      'When caregivers are on sick leave, you need to make sure someone else can provide patient care at short notice. Atto allows you to quickly see who’s nearby and available to work – without having to call around.',
+  },
+  {
+    icon: '',
+    title: 'Track the whereabouts of your caregivers during the day',
+    description:
+      'When caregivers are on sick leave, you need to make sure someone else can provide patient care at short notice. Atto allows you to quickly see who’s nearby and available to work – without having to call around.',
+  },
+  {
+    icon: '',
+    title: 'View map-based timelines of your caregivers’ movements ',
+    description:
+      'When caregivers are on sick leave, you need to make sure someone else can provide patient care at short notice. Atto allows you to quickly see who’s nearby and available to work – without having to call around.',
+  },
+];
+
+export const accordionList2 = [
+  {
+    icon: '',
+    title: 'Forecast your payroll costs ahead of payday to avoid unwanted surprises',
+    description:
+      'Add your caregivers’ pay rates and automatically estimate their wages based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week!',
+  },
+  {
+    icon: '',
+    title: 'Compare the costs and time of different teams and different jobs',
+    description:
+      'Add your caregivers’ pay rates and automatically estimate their wages based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week!',
+  },
+  {
+    icon: '',
+    title: 'Spend less time on payroll, and more time with patients',
+    description:
+      'Add your caregivers’ pay rates and automatically estimate their wages based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week!',
+  },
+];
+
+export const oldList = [
+  'Paper timesheets get lost or damaged outdoors.',
+  'No easy way to verify time-tracking records.',
+  'Time records often get rounded or “guesstimated” resulting in time wastage and additional payroll costs.',
+  'Estimating the labor costs of each job is time-consuming.',
+  'Finding out where your crew are and what they’re working on requires lots of back-and-forth phone calls.',
+  'Lots of admin time is required to collect timesheets, verify records, and enter them into your payroll system.',
+];
+
+export const newList = [
+  'Time-tracking data is recorded electronically, and timesheets are automatically created for you.',
+  'Using GPS tracking, Atto can provide daily movement summaries of each worker.',
+  'Atto tracks time to the nearest second and allows you to restrict your crew from clocking-in until they’re at the job site.',
+  'You can input your crews’ individual pay rates and categorize their work using job codes, so you can see if projects are on budget.',
+  'You can log in to Atto and see where all your crew are in real-time.',
+  'Time tracking data is verified, and timesheets are generated automatically. All you have to do is input the data from Atto into your payroll system.',
+];
+
+export const steps = [
+  {
+    image: '',
+    title: '1. Download Atto',
+    description: 'Download Atto and create an account. Available on the web, iOS and Android.',
+  },
+  {
+    image: '',
+    title: '2. Invite team members',
+    description:
+      'Invite your field workers, site managers, and sub-contractors via email or invite link.',
+  },
+  {
+    image: '',
+    title: '3. Ready to go!',
+    description: 'Once they download Atto, you’re ready to go!',
+  },
+];
--- src/pages/industries/home-and-healthcare/index.js
+++ src/pages/industries/home-and-healthcare/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import Divider from '@components/atoms/divider';
 import SEO from '@components/molecules/seo';
@@ -10,14 +11,26 @@ import Article from '@components/molecules/article';
 import Story from '@components/organisms/story';
 import IconCardList from '@components/organisms/icon-card-list';
 import IndustryMainCard from '@components/organisms/industry-main-card';
+import AccordionArticle from '@components/organisms/accordion-article';
+import OldVsNew from '@components/organisms/old-vs-new';
+import Steps from '@components/organisms/steps';
 
 import authorImage from '@images/authorImage.png';
 import image2 from '@images/time-tracking-healthcare@2x.png';
 
 import { container } from '@styles/main.module.scss';
-import { checkList1 } from '@data/industries/construction.js';
+import { background } from './home-and-healthcare.module.scss';
 
 import image1 from '@images/no-image.png';
+
+import {
+  checkList1,
+  oldList,
+  newList,
+  accordionList1,
+  accordionList2,
+  steps,
+} from '@data/industries/roofing.js';
 
 import { FooterLinks } from '@locale/en.js';
 import { firstList } from '@data/industries/roofing.js';
@@ -25,61 +38,92 @@ import { firstList } from '@data/industries/roofing.js';
 // import localeData from '@locale/en';
 
 const HomeHealthcare = () => (
-  <div className={`${container}`}>
-    <SEO title="Home and Healthcare Industries" />
-    <Header />
-    <IndustryMainCard
-      smallTitle="ATTO FOR HOME HEALTHCARE"
-      title="Time Tracking Software for Home Healthcare Providers"
-      list={checkList1}
-      image={image2}
-    />
-    <Divider className="style2" />
-    <Story
-      img={authorImage}
-      paragraph={`"Does precisely and perfectly what it says. The great big pulsating green button is appealing, makes you want to start work right away! Loving it and the helpful staff."
+  <>
+    <div className={background}></div>
+    <div className={`${container}`}>
+      <SEO title="Home and Healthcare Industries" />
+      <Header />
+      <IndustryMainCard
+        smallTitle="ATTO FOR HOME HEALTHCARE"
+        title="Time Tracking Software for Home Healthcare Providers"
+        list={checkList1}
+        image={image2}
+      />
+      <Divider className="style2" />
+      <Story
+        img={authorImage}
+        paragraph={`"Does precisely and perfectly what it says. The great big pulsating green button is appealing, makes you want to start work right away! Loving it and the helpful staff."
       `}
-      author="Robert Bennett - DPA Cleaning Services, Inc."
-    />
-    <Divider />
-    <Title
-      title="Keep track of your caregivers throughout their day"
-      description="With real-time activity tracking, you can check if your caregivers are working, on a break, or enjoying some time off."
-      maxWidth={920}
-      maxDescriptionWidth={800}
-    />
-    <Divider className="style2" />
-    <IconCardList cardList={firstList} hasBigImages style="smallerMargin" />
-    <Divider />
-    <Article
-      title="Get helpful reminders so your caregivers never forget to clock in!"
-      description="When caregivers are nursing patients, it can be easy to lose track of time. That’s why whenever they enter or leave a patient’s home, and at their normal start and end time, we’ll remind them to clock in and out. And if they still happen to forget, they can add a manual time entry for you to review later."
-      image={image1}
-      maxWidth={500}
-      imagePadding="7.3rem 4.5rem"
-      imageWidth={400}
-      imageHeight={354}
-    />
-    <Article
-      title="Entrust your senior caregivers to track everyone’s time"
-      description="Some patients with complex needs, or larger caring jobs may require multiple caregivers to work together in the same home. Instead of having each caregiver track their time individually, your senior caregiver can use Atto to clock in and out on all of their behalfs."
-      image={image1}
-      isSwapped
-      maxWidth={500}
-      imagePadding="7.3rem 4.5rem"
-      imageWidth={400}
-      imageHeight={354}
-    />
-    <SubscribeBanner
-      title="You provide quality care. We’ll provide quality time tracking software!"
-      placeholder="Type your email"
-      bannerImage
-      checkItemOne="No credit card required"
-      checkItemTwo="14 day free trial"
-      checkItemThree="Cancel anytime"
-    />
-    <Footer FooterLinks={FooterLinks} />
-  </div>
+        author="Robert Bennett - DPA Cleaning Services, Inc."
+      />
+      <Divider />
+      <Title
+        title="Keep track of your caregivers throughout their day"
+        description="With real-time activity tracking, you can check if your caregivers are working, on a break, or enjoying some time off."
+        maxWidth={920}
+        maxDescriptionWidth={800}
+      />
+      <Divider className="style2" />
+      <IconCardList cardList={firstList} hasBigImages style="smallerMargin" />
+      <Divider />
+      <Article
+        title="Get helpful reminders so your caregivers never forget to clock in!"
+        description="When caregivers are nursing patients, it can be easy to lose track of time. That’s why whenever they enter or leave a patient’s home, and at their normal start and end time, we’ll remind them to clock in and out. And if they still happen to forget, they can add a manual time entry for you to review later."
+        image={image1}
+        maxWidth={500}
+        imagePadding="7.3rem 4.5rem"
+        imageWidth={400}
+        imageHeight={354}
+      />
+      <Article
+        title="Entrust your senior caregivers to track everyone’s time"
+        description="Some patients with complex needs, or larger caring jobs may require multiple caregivers to work together in the same home. Instead of having each caregiver track their time individually, your senior caregiver can use Atto to clock in and out on all of their behalfs."
+        image={image1}
+        isSwapped
+        maxWidth={500}
+        imagePadding="7.3rem 4.5rem"
+        imageWidth={400}
+        imageHeight={354}
+      />
+      <Divider className="style3" />
+      <AccordionArticle
+        title="Keep track of your caregivers as they provide care throughout the day"
+        list={accordionList1}
+      />
+      <Divider />
+      <AccordionArticle
+        title="Everything you need to minimize admin and keep track of your budget"
+        list={accordionList2}
+        isSwapped
+      />
+      <Divider className="style11" />
+      <Title
+        title="Say goodbye to paper timesheets!"
+        description="Ditch paper timesheets and switch to Atto – an easier and more accurate time-tracking solution for roofing companies"
+        maxWidth={1040}
+        maxDescriptionWidth={900}
+      />
+      <Divider className="style5" />
+      <OldVsNew oldList={oldList} newList={newList} />
+      <Divider />
+      <StaticImage src="../../../images/paper_timesheets@2x.png" />
+      <Divider className="style1" />
+      <Steps list={steps} />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <SubscribeBanner
+        title="You provide quality care. We’ll provide quality time tracking software!"
+        placeholder="Type your email"
+        checkItemOne="No credit card required"
+        checkItemTwo="14 day free trial"
+        checkItemThree="Cancel anytime"
+      />
+      <Footer FooterLinks={FooterLinks} />
+    </div>
+  </>
 );
 
 export default HomeHealthcare;
