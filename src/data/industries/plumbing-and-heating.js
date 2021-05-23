235f1039cebb383ef50450fc5a4e0472b0e25f51
Enis <enisk@starlabspro.com>
2021-05-23 20:00:30 +0200

    Added components at plumbers and heating

---
+++ src/data/industries/plumbing-and-heating.js
@@ -0,0 +1,103 @@
+export const checkList1 = [
+  { title: 'Keep accurate time records while working outdoors.' },
+  { title: 'Track your roofers’ time and location in real-time.' },
+  { title: 'Respond quickly to changes in the weather.' },
+  { title: 'Improve the accuracy of quotes to win more jobs.' },
+];
+
+export const accordionList1 = [
+  {
+    icon: '',
+    title: 'Respond faster to emergency plumbing call-outs',
+    description:
+      'Burst pipes flooding a home? Attending a water leak that’s got out of hand? When you receive emergency call-outs, you need to be able to respond as quickly as possible. Instantly, Atto shows you which plumbers are nearby and available to work – without having to call around.',
+  },
+  {
+    icon: '',
+    title: 'Stop plumbers from clocking in until they arrive at the job',
+    description:
+      'Burst pipes flooding a home? Attending a water leak that’s got out of hand? When you receive emergency call-outs, you need to be able to respond as quickly as possible. Instantly, Atto shows you which plumbers are nearby and available to work – without having to call around.',
+  },
+  {
+    icon: '',
+    title: 'Track the locations of your plumbers and progress of jobs in real-time',
+    description:
+      'Burst pipes flooding a home? Attending a water leak that’s got out of hand? When you receive emergency call-outs, you need to be able to respond as quickly as possible. Instantly, Atto shows you which plumbers are nearby and available to work – without having to call around.',
+  },
+  {
+    icon: '',
+    title: 'View a map-based timeline of each plumber’s movements ',
+    description:
+      'Burst pipes flooding a home? Attending a water leak that’s got out of hand? When you receive emergency call-outs, you need to be able to respond as quickly as possible. Instantly, Atto shows you which plumbers are nearby and available to work – without having to call around.',
+  },
+];
+
+export const accordionList2 = [
+  {
+    icon: '',
+    title: 'Avoid surprises by forecasting your wage bill ahead of payday',
+    description:
+      'Accurately estimate and forecast your payroll. With Atto, you can add your plumbers’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week.',
+  },
+  {
+    icon: '',
+    title: 'From plumbers to heating engineers, see how much work each team completed',
+    description:
+      'Accurately estimate and forecast your payroll. With Atto, you can add your plumbers’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week.',
+  },
+  {
+    icon: '',
+    title: 'Eliminate “guesstimates” to bill your customers more accurately',
+    description:
+      'Accurately estimate and forecast your payroll. With Atto, you can add your plumbers’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week.',
+  },
+  {
+    icon: '',
+    title: 'Reduce the admin time taken to run your payroll',
+    description:
+      'Accurately estimate and forecast your payroll. With Atto, you can add your plumbers’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week.',
+  },
+  {
+    icon: '',
+    title: 'Win more contracts with precise cost estimates',
+    description:
+      'Accurately estimate and forecast your payroll. With Atto, you can add your plumbers’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week.',
+  },
+];
+
+export const oldList = [
+  'Paper timesheets get lost or damaged outdoors.',
+  'No easy way to verify time-tracking records.',
+  'Time records often get rounded or “guesstimated” resulting in time wastage and additional payroll costs.',
+  'Estimating the labor costs of each job is time-consuming.',
+  'Finding out where your crew are and what they’re working on requires lots of back-and-forth phone calls.',
+  'Lots of admin time is required to collect timesheets, verify records, and enter them into your payroll system.',
+];
+
+export const newList = [
+  'Time-tracking data is recorded electronically, and timesheets are automatically created for you.',
+  'Using GPS tracking, Atto can provide daily movement summaries of each worker.',
+  'Atto tracks time to the nearest second and allows you to restrict your crew from clocking-in until they’re at the job site.',
+  'You can input your crews’ individual pay rates and categorize their work using job codes, so you can see if projects are on budget.',
+  'You can log in to Atto and see where all your crew are in real-time.',
+  'Time tracking data is verified, and timesheets are generated automatically. All you have to do is input the data from Atto into your payroll system.',
+];
+
+export const steps = [
+  {
+    image: '',
+    title: '1. Download Atto',
+    description: 'Download Atto and create an account. Available on the web, iOS and Android.',
+  },
+  {
+    image: '',
+    title: '2. Invite team members',
+    description:
+      'Invite your field workers, site managers, and sub-contractors via email or invite link.',
+  },
+  {
+    image: '',
+    title: '3. Ready to go!',
+    description: 'Once they download Atto, you’re ready to go!',
+  },
+];
--- src/pages/industries/plumbing-and-heating/index.js
+++ src/pages/industries/plumbing-and-heating/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import Divider from '@components/atoms/divider';
 import SEO from '@components/molecules/seo';
@@ -10,13 +11,25 @@ import SubscribeBanner from '@components/molecules/subscribe-banner';
 import Story from '@components/organisms/story';
 import IconCardList from '@components/organisms/icon-card-list';
 import IndustryMainCard from '@components/organisms/industry-main-card';
+import AccordionArticle from '@components/organisms/accordion-article';
+import OldVsNew from '@components/organisms/old-vs-new';
+import Steps from '@components/organisms/steps';
 
 import authorImage from '@images/authorImage.png';
 import { container } from '@styles/main.module.scss';
-import { checkList1 } from '@data/industries/construction.js';
+import {
+  checkList1,
+  oldList,
+  newList,
+  accordionList1,
+  accordionList2,
+  steps,
+} from '@data/industries/plumbing-and-heating.js';
 
 import image1 from '@images/no-image.png';
 import image2 from '@images/time-tracking-plumbing@2x.png';
+
+import { background } from './plumbing-and-heating.module.scss';
 
 import { FooterLinks } from '@locale/en.js';
 import { firstList } from '@data/industries/roofing.js';
@@ -24,59 +37,92 @@ import { firstList } from '@data/industries/roofing.js';
 // import localeData from '@locale/en';
 
 const PlumbingAndHeating = () => (
-  <div className={`${container}`}>
-    <SEO title="Plumbing And Heating Industries" />
-    <Header />
-    <IndustryMainCard
-      smallTitle="ATTO FOR PLUMBERS AND HEATING ENGINEERS"
-      title="Time Tracking for Plumbing and Heating Companies"
-      list={checkList1}
-      image={image2}
-    />
-    <Divider className="style2" />
-    <Story
-      img={authorImage}
-      paragraph={`"Does precisely and perfectly what it says. The great big pulsating green button is appealing, makes you want to start work right away! Loving it and the helpful staff."
+  <>
+    <div className={background}></div>
+    <div className={`${container}`}>
+      <SEO title="Plumbing And Heating Industries" />
+      <Header />
+      <IndustryMainCard
+        smallTitle="ATTO FOR PLUMBERS AND HEATING ENGINEERS"
+        title="Time Tracking for Plumbing and Heating Companies"
+        list={checkList1}
+        image={image2}
+      />
+      <Divider className="style2" />
+      <Story
+        img={authorImage}
+        paragraph={`"Does precisely and perfectly what it says. The great big pulsating green button is appealing, makes you want to start work right away! Loving it and the helpful staff."
       `}
-      author="Robert Bennett - DPA Cleaning Services, Inc."
-    />
-    <Divider />
-    <Title
-      title="Track all of your plumbers throughout their workday"
-      description="With real-time activity tracking, you can check if your plumbers are working, on a break, or enjoying some time off."
-    />
-    <Divider />
-    <IconCardList cardList={firstList} hasBigImages style="smallerMargin" />
-    <Divider />
-    <Article
-      title="Get helpful reminders so your plumbers never forget to clock in!"
-      description="When dealing with burst pipes and emergency call-outs, it’s easy to lose track of time. That’s why when your plumbers enter or leave a job site, and at their normal start and end time, we’ll remind them to clock in and out. And if they happen to forget, they can add a manual time entry for you to review later."
-      image={image1}
-      maxWidth={500}
-      imagePadding="7.3rem 4.5rem"
-      imageWidth={400}
-      imageHeight={354}
-    />
-    <Article
-      title="Entrust your on-site manager to track your plumbers’ time"
-      description="Dealing with commercial or industrial plumbing jobs that require multiple plumbers on-site? Instead of having each plumber track their time individually, your on-the-job manager can clock in and out on all of your plumbers’ behalf."
-      image={image1}
-      isSwapped
-      maxWidth={500}
-      imagePadding="7.3rem 4.5rem"
-      imageWidth={400}
-      imageHeight={354}
-    />
-    <SubscribeBanner
-      title="All cisterns go! Get to work with accurate time tracking from Atto."
-      placeholder="Type your email"
-      bannerImage
-      checkItemOne="No credit card required"
-      checkItemTwo="14 day free trial"
-      checkItemThree="Cancel anytime"
-    />
-    <Footer FooterLinks={FooterLinks} />
-  </div>
+        author="Robert Bennett - DPA Cleaning Services, Inc."
+      />
+      <Divider />
+      <Title
+        title="Track all of your plumbers throughout their workday"
+        description="With real-time activity tracking, you can check if your plumbers are working, on a break, or enjoying some time off."
+        maxWidth={920}
+        maxDescriptionWidth={800}
+      />
+      <Divider />
+      <IconCardList cardList={firstList} hasBigImages style="smallerMargin" />
+      <Divider />
+      <Article
+        title="Get helpful reminders so your plumbers never forget to clock in!"
+        description="When dealing with burst pipes and emergency call-outs, it’s easy to lose track of time. That’s why when your plumbers enter or leave a job site, and at their normal start and end time, we’ll remind them to clock in and out. And if they happen to forget, they can add a manual time entry for you to review later."
+        image={image1}
+        maxWidth={500}
+        imagePadding="7.3rem 4.5rem"
+        imageWidth={400}
+        imageHeight={354}
+      />
+      <Article
+        title="Entrust your on-site manager to track your plumbers’ time"
+        description="Dealing with commercial or industrial plumbing jobs that require multiple plumbers on-site? Instead of having each plumber track their time individually, your on-the-job manager can clock in and out on all of your plumbers’ behalf."
+        image={image1}
+        isSwapped
+        maxWidth={500}
+        imagePadding="7.3rem 4.5rem"
+        imageWidth={400}
+        imageHeight={354}
+      />
+      <Divider className="style3" />
+      <AccordionArticle
+        title="Keep track of your plumbers as they make their way to different call-outs throughout the day"
+        list={accordionList1}
+      />
+      <Divider />
+      <AccordionArticle
+        title="Keep track of your plumbers as they make their way to different call-outs throughout the day"
+        list={accordionList2}
+        isSwapped
+      />
+      <Divider className="style11" />
+      <Title
+        title="Say goodbye to paper timesheets!"
+        description="Ditch paper timesheets and switch to Atto – an easier and more accurate time-tracking solution for roofing companies"
+        maxWidth={1040}
+        maxDescriptionWidth={900}
+      />
+      <Divider className="style5" />
+      <OldVsNew oldList={oldList} newList={newList} />
+      <Divider />
+      <StaticImage src="../../../images/paper_timesheets@2x.png" />
+      <Divider className="style1" />
+      <Steps list={steps} />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <SubscribeBanner
+        title="All cisterns go! Get to work with accurate time tracking from Atto."
+        placeholder="Type your email"
+        checkItemOne="No credit card required"
+        checkItemTwo="14 day free trial"
+        checkItemThree="Cancel anytime"
+      />
+      <Footer FooterLinks={FooterLinks} />
+    </div>
+  </>
 );
 
 export default PlumbingAndHeating;
