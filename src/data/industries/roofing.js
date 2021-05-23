7c73ce52bbc0c8de31168786030eccacb3985bcf
Enis <enisk@starlabspro.com>
2021-05-23 19:02:47 +0200

    Added roofing page data

--- src/data/industries/roofing.js
+++ src/data/industries/roofing.js
@@ -5,6 +5,13 @@ import icon4 from '@images/manage-overtime.png';
 import icon5 from '@images/switch-between-jobs.png';
 import icon6 from '@images/add-time-off.png';
 
+export const checkList1 = [
+  { title: 'Keep accurate time records while working outdoors.' },
+  { title: 'Track your roofers’ time and location in real-time.' },
+  { title: 'Respond quickly to changes in the weather.' },
+  { title: 'Improve the accuracy of quotes to win more jobs.' },
+];
+
 export const firstList = [
   {
     title: 'Clock in and out',
@@ -53,5 +60,96 @@ export const firstList = [
     icon: icon6,
     imageWidth: 350,
     imageHeight: 244,
+  },
+];
+
+export const accordionList1 = [
+  {
+    icon: '',
+    title: 'Respond quickly to weather changes and employee absences',
+    description:
+      'The unpredictability of the weather and employee absences mean you may need to change your plans at short notice. Atto allows you to instantly see which of your roofing contractors are nearby and available to work – without having to call around.',
+  },
+  {
+    icon: '',
+    title: 'Stop roofers from clocking in until they’re ready to work',
+    description:
+      'The unpredictability of the weather and employee absences mean you may need to change your plans at short notice. Atto allows you to instantly see which of your roofing contractors are nearby and available to work – without having to call around.',
+  },
+  {
+    icon: '',
+    title: 'Track the locations of your roofers and the progress of roofing jobs in real-time',
+    description:
+      'The unpredictability of the weather and employee absences mean you may need to change your plans at short notice. Atto allows you to instantly see which of your roofing contractors are nearby and available to work – without having to call around.',
+  },
+  {
+    icon: '',
+    title: 'View a map-based timeline of your roofers’ movements',
+    description:
+      'The unpredictability of the weather and employee absences mean you may need to change your plans at short notice. Atto allows you to instantly see which of your roofing contractors are nearby and available to work – without having to call around.',
+  },
+];
+
+export const accordionList2 = [
+  {
+    icon: '',
+    title: 'Forecast your wage bill ahead of payday to avoid unwanted surprises',
+    description:
+      'Add your roofers’ pay rates and automatically estimate their wages based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week!',
+  },
+  {
+    icon: '',
+    title: 'From roof tilers to safety inspectors see how much work each team’s completed',
+    description:
+      'Add your roofers’ pay rates and automatically estimate their wages based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week!',
+  },
+  {
+    icon: '',
+    title: 'Reduce the admin time it takes to run your payroll',
+    description:
+      'Add your roofers’ pay rates and automatically estimate their wages based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week!',
+  },
+  {
+    icon: '',
+    title: 'Win more roofing contracts with competitive quotes',
+    description:
+      'Add your roofers’ pay rates and automatically estimate their wages based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week!',
+  },
+];
+
+export const oldList = [
+  'Paper timesheets get lost or damaged outdoors.',
+  'No easy way to verify time-tracking records.',
+  'Time records often get rounded or “guesstimated” resulting in time wastage and additional payroll costs.',
+  'Estimating the labor costs of each job is time-consuming.',
+  'Finding out where your crew are and what they’re working on requires lots of back-and-forth phone calls.',
+  'Lots of admin time is required to collect timesheets, verify records, and enter them into your payroll system.',
+];
+
+export const newList = [
+  'Time-tracking data is recorded electronically, and timesheets are automatically created for you.',
+  'Using GPS tracking, Atto can provide daily movement summaries of each worker.',
+  'Atto tracks time to the nearest second and allows you to restrict your crew from clocking-in until they’re at the job site.',
+  'You can input your crews’ individual pay rates and categorize their work using job codes, so you can see if projects are on budget.',
+  'You can log in to Atto and see where all your crew are in real-time.',
+  'Time tracking data is verified, and timesheets are generated automatically. All you have to do is input the data from Atto into your payroll system.',
+];
+
+export const steps = [
+  {
+    image: '',
+    title: '1. Download Atto',
+    description: 'Download Atto and create an account. Available on the web, iOS and Android.',
+  },
+  {
+    image: '',
+    title: '2. Invite team members',
+    description:
+      'Invite your field workers, site managers, and sub-contractors via email or invite link.',
+  },
+  {
+    image: '',
+    title: '3. Ready to go!',
+    description: 'Once they download Atto, you’re ready to go!',
   },
 ];
--- src/pages/industries/roofing/index.js
+++ src/pages/industries/roofing/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import Divider from '@components/atoms/divider';
 import SEO from '@components/molecules/seo';
@@ -10,77 +11,115 @@ import Article from '@components/molecules/article';
 import SubscribeBanner from '@components/molecules/subscribe-banner';
 import IconCardList from '@components/organisms/icon-card-list';
 import IndustryMainCard from '@components/organisms/industry-main-card';
+import AccordionArticle from '@components/organisms/accordion-article';
+import OldVsNew from '@components/organisms/old-vs-new';
+import Steps from '@components/organisms/steps';
 
 import authorImage from '@images/authorImage.png';
 import image1 from '@images/no-image.png';
 import image2 from '@images/time-tracking-roofing@2x.png';
 
 import { container } from '@styles/main.module.scss';
-import { checkList1 } from '@data/industries/construction.js';
+import { background } from './roofing.module.scss';
+
+import {
+  checkList1,
+  oldList,
+  newList,
+  firstList,
+  accordionList1,
+  accordionList2,
+  steps,
+} from '@data/industries/roofing.js';
 
 import { FooterLinks } from '@locale/en.js';
-import { firstList } from '@data/industries/roofing.js';
 
 // import localeData from '@locale/en';
 
 const Roofing = () => (
-  <div className={`${container}`}>
-    <SEO title="Roofing Industries" />
-    <Header />
-    <IndustryMainCard
-      smallTitle="ATTO FOR ROOFING"
-      title="Time Tracking for Roofing Companies"
-      list={checkList1}
-      image={image2}
-    />
-    <Divider />
-    <Title title="Built with Roofing companies in mind" maxWidth={600} />
-    <Divider className="style2" />
-    <Story
-      img={authorImage}
-      paragraph="Does precisely and perfectly what it says. The great big pulsating green button is appealing, makes you want to start work right away! Loving it and the helpful staff."
-      author="Robert Bennett - DPA Cleaning Services, Inc."
-    />
-    <Divider />
-    <Title
-      title="Track all of your roofing contractors throughout their workday"
-      maxWidth={920}
-      maxDescriptionWidth={800}
-      description="With real-time activity tracking, you can check if your roofers are working, on a break, or enjoying some time off."
-    />
-    <Divider className="style3" />
-    <IconCardList cardList={firstList} hasBigImages style="smallerMargin" />
-    <Divider />
-    <Article
-      title="Get helpful reminders so your roofers never forget to clock in!"
-      description="When your roofers are in the middle of a job, it can be easy to lose track of time. That’s why whenever they enter or leave a job site, and at their normal start and end time, we’ll remind them to clock in and out. And if they happen to forget, they can add a manual time entry for you to review later."
-      image={image1}
-      maxWidth={500}
-      imagePadding="7.3rem 4.5rem"
-      imageWidth={400}
-      imageHeight={354}
-    />
-    <Article
-      title="Entrust your supervisor to track your roofing crew’s time"
-      description="Tiling large buildings or doing commercial and industrial roofing jobs require lots of your roofers to work together. Instead of having each contractor track their time individually, your on-the-job supervisor can use Atto to clock-in and out on their behalf."
-      image={image1}
-      isSwapped
-      maxWidth={500}
-      imagePadding="7.3rem 4.5rem"
-      imageWidth={400}
-      imageHeight={354}
-    />
-    <Divider />
-    <SubscribeBanner
-      title="Time tracking for roofing companies? We’re on top of it!"
-      placeholder="Type your email"
-      bannerImage
-      checkItemOne="No credit card required"
-      checkItemTwo="14 day free trial"
-      checkItemThree="Cancel anytime"
-    />
-    <Footer FooterLinks={FooterLinks} />
-  </div>
+  <>
+    <div className={background}></div>
+    <div className={`${container}`}>
+      <SEO title="Roofing Industries" />
+      <Header />
+      <IndustryMainCard
+        smallTitle="ATTO FOR ROOFING"
+        title="Time Tracking for Roofing Companies"
+        list={checkList1}
+        image={image2}
+      />
+      <Divider className="style3" />
+      <Story
+        img={authorImage}
+        paragraph="Does precisely and perfectly what it says. The great big pulsating green button is appealing, makes you want to start work right away! Loving it and the helpful staff."
+        author="Robert Bennett - DPA Cleaning Services, Inc."
+      />
+      <Divider />
+      <Title
+        title="Track all of your roofing contractors throughout their workday"
+        maxWidth={920}
+        maxDescriptionWidth={800}
+        description="With real-time activity tracking, you can check if your roofers are working, on a break, or enjoying some time off."
+      />
+      <Divider className="style3" />
+      <IconCardList cardList={firstList} hasBigImages style="smallerMargin" />
+      <Divider />
+      <Article
+        title="Get helpful reminders so your roofers never forget to clock in!"
+        description="When your roofers are in the middle of a job, it can be easy to lose track of time. That’s why whenever they enter or leave a job site, and at their normal start and end time, we’ll remind them to clock in and out. And if they happen to forget, they can add a manual time entry for you to review later."
+        image={image1}
+        maxWidth={500}
+        imagePadding="7.3rem 4.5rem"
+        imageWidth={400}
+        imageHeight={354}
+      />
+      <Article
+        title="Entrust your supervisor to track your roofing crew’s time"
+        description="Tiling large buildings or doing commercial and industrial roofing jobs require lots of your roofers to work together. Instead of having each contractor track their time individually, your on-the-job supervisor can use Atto to clock-in and out on their behalf."
+        image={image1}
+        isSwapped
+        maxWidth={500}
+        imagePadding="7.3rem 4.5rem"
+        imageWidth={400}
+        imageHeight={354}
+      />
+      <Divider className="style3" />
+      <AccordionArticle
+        title="Keep track of your roofing contractors as they work throughout the day"
+        list={accordionList1}
+      />
+      <Divider />
+      <AccordionArticle
+        title="Everything you need to keep roofing jobs on schedule and on budget"
+        list={accordionList2}
+        isSwapped
+      />
+      <Divider className="style11" />
+      <Title
+        title="Say goodbye to paper timesheets!"
+        description="Ditch paper timesheets and switch to Atto – an easier and more accurate time-tracking solution for roofing companies"
+      />
+      <Divider className="style5" />
+      <OldVsNew oldList={oldList} newList={newList} />
+      <Divider />
+      <StaticImage src="../../../images/paper_timesheets@2x.png" />
+      <Divider className="style1" />
+      <Steps list={steps} />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <SubscribeBanner
+        title="Time tracking for roofing companies? We’re on top of it!"
+        placeholder="Type your email"
+        checkItemOne="No credit card required"
+        checkItemTwo="14 day free trial"
+        checkItemThree="Cancel anytime"
+      />
+      <Footer FooterLinks={FooterLinks} />
+    </div>
+  </>
 );
 
 export default Roofing;
