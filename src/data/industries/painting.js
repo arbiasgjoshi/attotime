17f71ceff539e35e9781fd731617f9bc9fecd355
Enis <enisk@starlabspro.com>
2021-05-23 19:25:52 +0200

    Added painting and decorating data

---
+++ src/data/industries/painting.js
@@ -0,0 +1,103 @@
+export const checkList1 = [
+  { title: 'Track your painters’ and decorators’ time and location.' },
+  { title: 'See who’s available to work in real-time.' },
+  { title: 'Bill customers for every minute spent on the job.' },
+  { title: 'Accurately estimate your labor costs to win more jobs.' },
+];
+
+export const accordionList1 = [
+  {
+    icon: '',
+    title: 'Respond to absenteeism and last-minute schedule changes',
+    description:
+      'When a van breaks down, or a painter calls-in sick, you need to be able to deploy other workers to finish the job. Instantly, Atto shows you which of your painters and decorators are nearby and available to work – without having to call around.',
+  },
+  {
+    icon: '',
+    title: 'Stop painters from clocking in until they’re ready to work',
+    description:
+      'When a van breaks down, or a painter calls-in sick, you need to be able to deploy other workers to finish the job. Instantly, Atto shows you which of your painters and decorators are nearby and available to work – without having to call around.',
+  },
+  {
+    icon: '',
+    title: 'Track the locations of your workers and the progress of paint jobs in real-time',
+    description:
+      'When a van breaks down, or a painter calls-in sick, you need to be able to deploy other workers to finish the job. Instantly, Atto shows you which of your painters and decorators are nearby and available to work – without having to call around.',
+  },
+  {
+    icon: '',
+    title: 'View a map-based timeline of your painters’ and decorators’ daily movements ',
+    description:
+      'When a van breaks down, or a painter calls-in sick, you need to be able to deploy other workers to finish the job. Instantly, Atto shows you which of your painters and decorators are nearby and available to work – without having to call around.',
+  },
+];
+
+export const accordionList2 = [
+  {
+    icon: '',
+    title: 'Avoid surprises by forecasting wages ahead of payday',
+    description:
+      'Add your painters’ and decorators’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week!',
+  },
+  {
+    icon: '',
+    title: 'From painting to decorating, see how much work each team completed',
+    description:
+      'Add your painters’ and decorators’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week!',
+  },
+  {
+    icon: '',
+    title: 'Eliminate “guesstimates” to bill your customers more accurately',
+    description:
+      'Add your painters’ and decorators’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week!',
+  },
+  {
+    icon: '',
+    title: 'Reduce the admin time taken to run your payroll',
+    description:
+      'Add your painters’ and decorators’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week!',
+  },
+  {
+    icon: '',
+    title: 'Win high-value jobs with more competitive quotes',
+    description:
+      'Add your painters’ and decorators’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week!',
+  },
+];
+
+export const oldList = [
+  'Paper timesheets get lost or damaged outdoors.',
+  'No easy way to verify time-tracking records.',
+  'Time records often get rounded or “guesstimated” resulting in time wastage and additional payroll costs.',
+  'Estimating the labor costs of each job is time-consuming.',
+  'Finding out where your crew are and what they’re working on requires lots of back-and-forth phone calls.',
+  'Lots of admin time is required to collect timesheets, verify records, and enter them into your payroll system.',
+];
+
+export const newList = [
+  'Time-tracking data is recorded electronically, and timesheets are automatically created for you.',
+  'Using GPS tracking, Atto can provide daily movement summaries of each worker.',
+  'Atto tracks time to the nearest second and allows you to restrict your crew from clocking-in until they’re at the job site.',
+  'You can input your crews’ individual pay rates and categorize their work using job codes, so you can see if projects are on budget.',
+  'You can log in to Atto and see where all your crew are in real-time.',
+  'Time tracking data is verified, and timesheets are generated automatically. All you have to do is input the data from Atto into your payroll system.',
+];
+
+export const steps = [
+  {
+    image: '',
+    title: '1. Download Atto',
+    description: 'Download Atto and create an account. Available on the web, iOS and Android.',
+  },
+  {
+    image: '',
+    title: '2. Invite team members',
+    description:
+      'Invite your field workers, site managers, and sub-contractors via email or invite link.',
+  },
+  {
+    image: '',
+    title: '3. Ready to go!',
+    description: 'Once they download Atto, you’re ready to go!',
+  },
+];
---
+++ src/pages/industries/painting-and-decorating/index.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+
+import Divider from '@components/atoms/divider';
+import SEO from '@components/molecules/seo';
+import Title from '@components/molecules/title';
+import Header from '@components/molecules/header';
+import Footer from '@components/molecules/footer';
+import Article from '@components/molecules/article';
+import Story from '@components/organisms/story';
+import IconCardList from '@components/organisms/icon-card-list';
+import IndustryMainCard from '@components/organisms/industry-main-card';
+import AccordionArticle from '@components/organisms/accordion-article';
+import OldVsNew from '@components/organisms/old-vs-new';
+import Steps from '@components/organisms/steps';
+
+import SubscribeBanner from '@components/molecules/subscribe-banner';
+import { container } from '@styles/main.module.scss';
+import authorImage from '@images/authorImage.png';
+
+import image1 from '@images/no-image.png';
+import image2 from '@images/time-tracking-painting@2x.png';
+
+import {
+  checkList1,
+  oldList,
+  newList,
+  accordionList1,
+  accordionList2,
+  steps,
+} from '@data/industries/painting.js';
+
+import { FooterLinks } from '@locale/en.js';
+import { firstList } from '@data/industries/roofing.js';
+
+import { background } from './painting.module.scss';
+
+// import localeData from '@locale/en';
+
+const Painting = () => (
+  <>
+    <div className={background}></div>
+    <div className={`${container}`}>
+      <SEO title="Painting Industries" />
+      <Header />
+      <IndustryMainCard
+        smallTitle="ATTO FOR PAINTERS AND DECORATORS"
+        title="Time Tracking for Painting and Decorating Companies"
+        list={checkList1}
+        image={image2}
+      />
+      <Divider className="style3" />
+      <Story
+        img={authorImage}
+        paragraph={`"Does precisely and perfectly what it says. The great big pulsating green button is appealing, makes you want to start work right away! Loving it and the helpful staff."
+      `}
+        author="Robert Bennett - DPA Cleaning Services, Inc."
+      />
+      <Divider />
+      <Title
+        title="Track all of your painters and decorators throughout their workday"
+        description="With real-time activity tracking, you can check if your crew is working, on a break, or enjoying some time off."
+        maxWidth={920}
+        maxDescriptionWidth={800}
+      />
+      <Divider />
+      <IconCardList cardList={firstList} hasBigImages style="smallerMargin" />
+      <Divider />
+      <Article
+        title="Allow your supervisor to track your painting crew’s time"
+        description="Painting large properties or doing a commercial renovation requires your crew to work together. Instead of having each painter or decorator track their time individually, your on-the-job supervisor can use Atto to clock in and out on their behalf."
+        image={image1}
+        maxWidth={500}
+        imagePadding="7.3rem 4.5rem"
+        imageWidth={400}
+        imageHeight={354}
+      />
+      <Article
+        title="Entrust your supervisor to track your painting crew’s time"
+        description="Tiling large buildings or doing commercial and industrial painting jobs require lots of your roofers to work together. Instead of having each contractor track their time individually, your on-the-job supervisor can use Atto to clock-in and out on their behalf."
+        image={image1}
+        isSwapped
+        maxWidth={500}
+        imagePadding="7.3rem 4.5rem"
+        imageWidth={400}
+        imageHeight={354}
+      />
+      <Divider className="style3" />
+      <AccordionArticle
+        title="Keep track of your painting and decorating crew as they work throughout the day"
+        list={accordionList1}
+      />
+      <Divider />
+      <AccordionArticle
+        title="Everything you need to keep painting and decorating jobs on schedule and on budget"
+        list={accordionList2}
+        isSwapped
+      />
+      <Divider className="style11" />
+      <Title
+        title="Say goodbye to paper timesheets!"
+        description="Ditch paper timesheets and switch to Atto – an easier and more accurate time-tracking solution for roofing companies"
+        maxWidth={1040}
+        maxDescriptionWidth={900}
+      />
+      <Divider className="style5" />
+      <OldVsNew oldList={oldList} newList={newList} />
+      <Divider />
+      <StaticImage src="../../../images/paper_timesheets@2x.png" />
+      <Divider className="style1" />
+      <Steps list={steps} />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <SubscribeBanner
+        title="Using paper timesheets is like watching paint dry. Try Atto instead!"
+        placeholder="Type your email"
+        checkItemOne="No credit card required"
+        checkItemTwo="14 day free trial"
+        checkItemThree="Cancel anytime"
+      />
+      <Footer FooterLinks={FooterLinks} />
+    </div>
+  </>
+);
+
+export default Painting;
