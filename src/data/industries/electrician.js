52e91eb7bf98e791e87311130d6b104a2b0ce5cc
Enis <enisk@starlabspro.com>
2021-05-23 20:22:57 +0200

    Adding components at electrician

---
+++ src/data/industries/electrician.js
@@ -0,0 +1,103 @@
+export const checkList1 = [
+  { title: 'Track your electricians’ time and locations.' },
+  { title: 'Write job notes to complete inspection paperwork faster.' },
+  { title: 'Estimate your wage bill ahead of payday.' },
+  { title: 'Win valuable contracts with more accurate job quotes.' },
+];
+
+export const accordionList1 = [
+  {
+    icon: '',
+    title: 'Dispatch electricians to emergency call-outs faster',
+    description:
+      'Customer experiencing a power outage? Unsafe wiring about to give someone spiky hair? When you receive emergency call-outs, you need to be able to respond as quickly as possible. Atto allows you to instantly see which electricians are nearby and available to work – without needing to call around.',
+  },
+  {
+    icon: '',
+    title: 'Stop electricians from clocking in until they arrive on-site',
+    description:
+      'Customer experiencing a power outage? Unsafe wiring about to give someone spiky hair? When you receive emergency call-outs, you need to be able to respond as quickly as possible. Atto allows you to instantly see which electricians are nearby and available to work – without needing to call around.',
+  },
+  {
+    icon: '',
+    title: 'Track the locations of your electricians and progress of jobs in real-time',
+    description:
+      'Customer experiencing a power outage? Unsafe wiring about to give someone spiky hair? When you receive emergency call-outs, you need to be able to respond as quickly as possible. Atto allows you to instantly see which electricians are nearby and available to work – without needing to call around.',
+  },
+  {
+    icon: '',
+    title: 'View a map-based timeline of each electricians’ daily movements',
+    description:
+      'Customer experiencing a power outage? Unsafe wiring about to give someone spiky hair? When you receive emergency call-outs, you need to be able to respond as quickly as possible. Atto allows you to instantly see which electricians are nearby and available to work – without needing to call around.',
+  },
+];
+
+export const accordionList2 = [
+  {
+    icon: '',
+    title: 'Avoid a nasty shock by forecasting wages ahead of payday',
+    description:
+      'When you rely on manual timesheets, you can be in for a nasty shock if your payroll costs more than you budgeted. With Atto, you can add your electricians’ pay rates and automatically estimate their pay based on the hours they track. This means at any time you can login and get a real-time estimate of your current wage bill.',
+  },
+  {
+    icon: '',
+    title: 'Check if electrical jobs are running to budget with timesheet summaries',
+    description:
+      'When you rely on manual timesheets, you can be in for a nasty shock if your payroll costs more than you budgeted. With Atto, you can add your electricians’ pay rates and automatically estimate their pay based on the hours they track. This means at any time you can login and get a real-time estimate of your current wage bill.',
+  },
+  {
+    icon: '',
+    title: 'Pull the plug on “guesstimated” time and start invoicing for every minute worked',
+    description:
+      'When you rely on manual timesheets, you can be in for a nasty shock if your payroll costs more than you budgeted. With Atto, you can add your electricians’ pay rates and automatically estimate their pay based on the hours they track. This means at any time you can login and get a real-time estimate of your current wage bill.',
+  },
+  {
+    icon: '',
+    title: 'Cut the admin time taken to run your payroll',
+    description:
+      'When you rely on manual timesheets, you can be in for a nasty shock if your payroll costs more than you budgeted. With Atto, you can add your electricians’ pay rates and automatically estimate their pay based on the hours they track. This means at any time you can login and get a real-time estimate of your current wage bill.',
+  },
+  {
+    icon: '',
+    title: 'Win valuable contracts with precise cost estimates ',
+    description:
+      'When you rely on manual timesheets, you can be in for a nasty shock if your payroll costs more than you budgeted. With Atto, you can add your electricians’ pay rates and automatically estimate their pay based on the hours they track. This means at any time you can login and get a real-time estimate of your current wage bill.',
+  },
+];
+
+export const oldList = [
+  'Paper timesheets get lost or damaged outdoors.',
+  'No easy way to verify time-tracking records.',
+  'Time records often get rounded or “guesstimated” resulting in time wastage and additional payroll costs.',
+  'Estimating the labor costs of each job is time-consuming.',
+  'Finding out where your crew are and what they’re working on requires lots of back-and-forth phone calls.',
+  'Lots of admin time is required to collect timesheets, verify records, and enter them into your payroll system.',
+];
+
+export const newList = [
+  'Time-tracking data is recorded electronically, and timesheets are automatically created for you.',
+  'Using GPS tracking, Atto can provide daily movement summaries of each worker.',
+  'Atto tracks time to the nearest second and allows you to restrict your crew from clocking-in until they’re at the job site.',
+  'You can input your crews’ individual pay rates and categorize their work using job codes, so you can see if projects are on budget.',
+  'You can log in to Atto and see where all your crew are in real-time.',
+  'Time tracking data is verified, and timesheets are generated automatically. All you have to do is input the data from Atto into your payroll system.',
+];
+
+export const steps = [
+  {
+    image: '',
+    title: '1. Download Atto',
+    description: 'Download Atto and create an account. Available on the web, iOS and Android.',
+  },
+  {
+    image: '',
+    title: '2. Invite team members',
+    description:
+      'Invite your field workers, site managers, and sub-contractors via email or invite link.',
+  },
+  {
+    image: '',
+    title: '3. Ready to go!',
+    description: 'Once they download Atto, you’re ready to go!',
+  },
+];
--- src/pages/industries/electrician/index.js
+++ src/pages/industries/electrician/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import Divider from '@components/atoms/divider';
 import SEO from '@components/molecules/seo';
@@ -10,76 +11,127 @@ import SubscribeBanner from '@components/molecules/subscribe-banner';
 import Story from '@components/organisms/story';
 import IconCardList from '@components/organisms/icon-card-list';
 import IndustryMainCard from '@components/organisms/industry-main-card';
+import AccordionArticle from '@components/organisms/accordion-article';
+import OldVsNew from '@components/organisms/old-vs-new';
+import Steps from '@components/organisms/steps';
 
 import authorImage from '@images/authorImage.png';
 import { container } from '@styles/main.module.scss';
+import { background } from './electrician.module.scss';
 
 import image1 from '@images/no-image.png';
 import image2 from '@images/time-tracking-electricians@2x.png';
 
 import { FooterLinks } from '@locale/en.js';
 import { firstList } from '@data/industries/roofing.js';
-import { checkList1 } from '@data/industries/construction.js';
+import {
+  checkList1,
+  oldList,
+  newList,
+  accordionList1,
+  accordionList2,
+  steps,
+} from '@data/industries/electrician.js';
 
 // import localeData from '@locale/en';
 
 const Electrician = () => (
-  <div className={`${container}`}>
-    <SEO title="Electrician Industries" />
-    <Header />
-    <IndustryMainCard
-      smallTitle="ATTO FOR ELECTRICIANS"
-      title="Time Tracking for Electricians"
-      list={checkList1}
-      image={image2}
-    />
-    <Title title="Built with Electrician companies in mind" maxWidth={600} />
-    <Divider className="style2" />
-    <Story
-      img={authorImage}
-      paragraph={`"Does precisely and perfectly what it says. The great big pulsating green button is appealing, makes you want to start work right away! Loving it and the helpful staff."
+  <>
+    <div className={background}></div>
+    <div className={`${container}`}>
+      <SEO title="Electrician Industries" />
+      <Header />
+      <IndustryMainCard
+        smallTitle="ATTO FOR ELECTRICIANS"
+        title="Time Tracking for Electricians"
+        list={checkList1}
+        image={image2}
+      />
+      <Divider className="style3" />
+      <Story
+        img={authorImage}
+        paragraph={`"Does precisely and perfectly what it says. The great big pulsating green button is appealing, makes you want to start work right away! Loving it and the helpful staff."
       `}
-      author="Robert Bennett - DPA Electrician Services, Inc."
-    />
-    <Divider />
-    <Title
-      title="Track all of your electricians throughout their workday"
-      maxWidth={920}
-      maxDescriptionWidth={800}
-      description="With real-time activity tracking, you can check if your electricians are working, on a break, or enjoying some time off."
-    />
-    <Divider />
-    <IconCardList cardList={firstList} hasBigImages style="smallerMargin" />
-    <Divider />
-    <Article
-      title="Give on-site supervisors the power to track your electricians’ time"
-      description="Wiring an entire building? Dealing with a commercial job that requires multiple electricians on-site? Instead of having each of them track their time individually, your on-site supervisor can clock-in and out on all of your electricians’ behalf."
-      image={image1}
-      maxWidth={500}
-      imagePadding="7.3rem 4.5rem"
-      imageWidth={400}
-      imageHeight={354}
-    />
-    <Article
-      title="Entrust managers to track your cleaners’ time"
-      description="Dealing with commercial or industrial Electrician jobs that require lots of cleaners? Instead of having each cleaner track their time individually, your on-the-job manager can clock in and out on your cleaner’s behalf."
-      image={image1}
-      isSwapped
-      maxWidth={500}
-      imagePadding="7.3rem 4.5rem"
-      imageWidth={400}
-      imageHeight={354}
-    />
-    <SubscribeBanner
-      title="Keep track of all your electricians in the field with Atto"
-      placeholder="Type your email"
-      bannerImage
-      checkItemOne="No credit card required"
-      checkItemTwo="14 day free trial"
-      checkItemThree="Cancel anytime"
-    />
-    <Footer FooterLinks={FooterLinks} />
-  </div>
+        author="Robert Bennett - DPA Electrician Services, Inc."
+      />
+      <Divider />
+      <Title
+        title="Track all of your electricians throughout their workday"
+        maxWidth={920}
+        maxDescriptionWidth={800}
+        description="With real-time activity tracking, you can check if your electricians are working, on a break, or enjoying some time off."
+      />
+      <Divider />
+      <IconCardList cardList={firstList} hasBigImages style="smallerMargin" />
+      <Divider />
+      <Article
+        title="Buzz your electricians with helpful reminders so they don’t forget to clock in!"
+        description="Handling electricity requires full focus and concentration, so it’s easy to lose track of time. That’s why whenever your electricians enter or leave a job site, and at their normal start and end time, we’ll remind them to clock in and out. 
+        And if they do happen to forget, they can add a manual time entry for you to review later."
+        image={image1}
+        maxWidth={500}
+        imagePadding="7.3rem 4.5rem"
+        imageWidth={400}
+        imageHeight={354}
+      />
+      <Article
+        title="Give on-site supervisors the power to track your electricians’ time"
+        description="Wiring an entire building? Dealing with a commercial job that requires multiple electricians on-site? Instead of having each of them track their time individually, your on-site supervisor can clock-in and out on all of your electricians’ behalf."
+        image={image1}
+        isSwapped
+        maxWidth={500}
+        imagePadding="7.3rem 4.5rem"
+        imageWidth={400}
+        imageHeight={354}
+      />
+      <Divider className="style3" />
+      <AccordionArticle
+        title="Keep track of your electricians as they make their way to different call-outs throughout the day"
+        list={accordionList1}
+      />
+      <Divider />
+      <AccordionArticle
+        title="Everything you need to keep electrical jobs on schedule and on budget"
+        list={accordionList2}
+        isSwapped
+      />
+      <Divider className="style11" />
+      <Title
+        title="Say goodbye to paper timesheets!"
+        description="Ditch paper timesheets and switch to Atto – an easier and more accurate time-tracking solution for roofing companies"
+        maxWidth={1040}
+        maxDescriptionWidth={900}
+      />
+      <Divider className="style5" />
+      <OldVsNew oldList={oldList} newList={newList} />
+      <Divider />
+      <StaticImage src="../../../images/paper_timesheets@2x.png" />
+      <Divider className="style1" />
+      <Title
+        title="Get everything set up in 3 simple steps"
+        description="Atto is designed to be the easiest time-tracking app to use.
+         There’s absolutely no technical knowledge required."
+        maxWidth={932}
+        maxDescriptionWidth={750}
+      />
+      <Divider className="style5" />
+      <Steps list={steps} />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <SubscribeBanner
+        title="Keep track of all your electricians in the field with Atto"
+        placeholder="Type your email"
+        bannerImage
+        checkItemOne="No credit card required"
+        checkItemTwo="14 day free trial"
+        checkItemThree="Cancel anytime"
+      />
+      <Footer FooterLinks={FooterLinks} />
+    </div>
+  </>
 );
 
 export default Electrician;
