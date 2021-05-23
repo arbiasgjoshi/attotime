d0085ac5bf1cba559d39f7a34dacf064b8dc4a40
Enis <enisk@starlabspro.com>
2021-05-23 20:10:35 +0200

    Added cleaning components

---
+++ src/data/industries/cleaning.js
@@ -0,0 +1,98 @@
+export const checkList1 = [
+  { title: 'Track your cleaners’ time and location in real-time.' },
+  { title: 'Increase the safety and accountability of your cleaners.' },
+  { title: 'Quickly deploy cleaners to urgent jobs.' },
+  { title: 'Estimate your labor costs to win more cleaning contracts.' },
+];
+
+export const accordionList1 = [
+  {
+    icon: '',
+    title: 'See who’s nearby and available to respond to urgent cleaning jobs',
+    description:
+      'Unexpected spillage reported? An incident that needs cleaning fast? When clients call with urgent work, you need to be able to respond quickly. Atto helps you instantly see which cleaners are nearby and available to work – without needing to call around.',
+  },
+  {
+    icon: '',
+    title: 'Avoid time waste by stopping cleaners from clocking in until they arrive on-site',
+    description:
+      'Unexpected spillage reported? An incident that needs cleaning fast? When clients call with urgent work, you need to be able to respond quickly. Atto helps you instantly see which cleaners are nearby and available to work – without needing to call around.',
+  },
+  {
+    icon: '',
+    title: 'Update clients in real-time on the progress of cleaning jobs ',
+    description:
+      'Unexpected spillage reported? An incident that needs cleaning fast? When clients call with urgent work, you need to be able to respond quickly. Atto helps you instantly see which cleaners are nearby and available to work – without needing to call around.',
+  },
+  {
+    icon: '',
+    title: 'Get daily summaries of each cleaner’s movements ',
+    description:
+      'Unexpected spillage reported? An incident that needs cleaning fast? When clients call with urgent work, you need to be able to respond quickly. Atto helps you instantly see which cleaners are nearby and available to work – without needing to call around.',
+  },
+];
+
+export const accordionList2 = [
+  {
+    icon: '',
+    title: 'Keep a handle on your biggest cost – your cleaners’ wages',
+    description:
+      'The biggest cost for any cleaning job is usually the wages of your cleaners – so it’s important for you to accurately estimate your payroll. With Atto, you can add your cleaners’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week.',
+  },
+  {
+    icon: '',
+    title:
+      'From window cleaners to domestic cleaners, keep tabs on how much work each team has done',
+    description:
+      'The biggest cost for any cleaning job is usually the wages of your cleaners – so it’s important for you to accurately estimate your payroll. With Atto, you can add your cleaners’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week.',
+  },
+  {
+    icon: '',
+    title: 'Eliminate “guesstimates” with quicker and more accurate payroll ',
+    description:
+      'The biggest cost for any cleaning job is usually the wages of your cleaners – so it’s important for you to accurately estimate your payroll. With Atto, you can add your cleaners’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week.',
+  },
+  {
+    icon: '',
+    title: 'Win more cleaning contracts with better cost estimates ',
+    description:
+      'The biggest cost for any cleaning job is usually the wages of your cleaners – so it’s important for you to accurately estimate your payroll. With Atto, you can add your cleaners’ pay rates and automatically estimate their pay based on the hours they track. Now, there’ll be no more payroll surprises at the end of the week.',
+  },
+];
+
+export const oldList = [
+  'Paper timesheets get lost or damaged outdoors.',
+  'No easy way to verify time-tracking records.',
+  'Time records often get rounded or “guesstimated” resulting in time wastage and additional payroll costs.',
+  'Estimating the labor costs of each job is time-consuming.',
+  'Finding out where your crew are and what they’re working on requires lots of back-and-forth phone calls.',
+  'Lots of admin time is required to collect timesheets, verify records, and enter them into your payroll system.',
+];
+
+export const newList = [
+  'Time-tracking data is recorded electronically, and timesheets are automatically created for you.',
+  'Using GPS tracking, Atto can provide daily movement summaries of each worker.',
+  'Atto tracks time to the nearest second and allows you to restrict your crew from clocking-in until they’re at the job site.',
+  'You can input your crews’ individual pay rates and categorize their work using job codes, so you can see if projects are on budget.',
+  'You can log in to Atto and see where all your crew are in real-time.',
+  'Time tracking data is verified, and timesheets are generated automatically. All you have to do is input the data from Atto into your payroll system.',
+];
+
+export const steps = [
+  {
+    image: '',
+    title: '1. Download Atto',
+    description: 'Download Atto and create an account. Available on the web, iOS and Android.',
+  },
+  {
+    image: '',
+    title: '2. Invite team members',
+    description:
+      'Invite your field workers, site managers, and sub-contractors via email or invite link.',
+  },
+  {
+    image: '',
+    title: '3. Ready to go!',
+    description: 'Once they download Atto, you’re ready to go!',
+  },
+];
--- src/pages/industries/cleaning/index.js
+++ src/pages/industries/cleaning/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import Divider from '@components/atoms/divider';
 import SEO from '@components/molecules/seo';
@@ -10,13 +11,25 @@ import SubscribeBanner from '@components/molecules/subscribe-banner';
 import Story from '@components/organisms/story';
 import IconCardList from '@components/organisms/icon-card-list';
 import IndustryMainCard from '@components/organisms/industry-main-card';
+import AccordionArticle from '@components/organisms/accordion-article';
+import OldVsNew from '@components/organisms/old-vs-new';
+import Steps from '@components/organisms/steps';
 
 import authorImage from '@images/authorImage.png';
 import { container } from '@styles/main.module.scss';
-import { checkList1 } from '@data/industries/construction.js';
+import {
+  checkList1,
+  oldList,
+  newList,
+  accordionList1,
+  accordionList2,
+  steps,
+} from '@data/industries/cleaning.js';
 
 import image1 from '@images/no-image.png';
 import image2 from '@images/time-tracking-cleaning@2x.png';
+
+import { background } from './cleaning.module.scss';
 
 import { FooterLinks } from '@locale/en.js';
 import { firstList } from '@data/industries/roofing.js';
@@ -24,62 +37,92 @@ import { firstList } from '@data/industries/roofing.js';
 // import localeData from '@locale/en';
 
 const Cleaning = () => (
-  <div className={`${container}`}>
-    <SEO title="Cleaning Industries" />
-    <Header />
-    <IndustryMainCard
-      smallTitle="ATTO FOR CLEANERS"
-      title="Time Tracking for Cleaning Companies"
-      list={checkList1}
-      image={image2}
-    />
-    <Title title="Built with Cleaning companies in mind" maxWidth={600} />
-    <Divider className="style2" />
-    <Story
-      img={authorImage}
-      paragraph={`"Does precisely and perfectly what it says. The great big pulsating green button is appealing, makes you want to start work right away! Loving it and the helpful staff."
+  <>
+    <div className={background}></div>
+    <div className={`${container}`}>
+      <SEO title="Cleaning Industries" />
+      <Header />
+      <IndustryMainCard
+        smallTitle="ATTO FOR CLEANERS"
+        title="Time Tracking for Cleaning Companies"
+        list={checkList1}
+        image={image2}
+      />
+      <Divider className="style3" />
+      <Story
+        img={authorImage}
+        paragraph={`"Does precisely and perfectly what it says. The great big pulsating green button is appealing, makes you want to start work right away! Loving it and the helpful staff."
       `}
-      author="Robert Bennett - DPA Cleaning Services, Inc."
-    />
-    <Divider />
-    <Title
-      title="Keep track of everything happening throughout your cleaners’ workday"
-      maxWidth={920}
-      maxDescriptionWidth={800}
-      description="Atto’s time tracking app is incredibly easy to use and allows you to see what all your cleaners are up to in real-time."
-    />
-    <Divider />
-    <IconCardList cardList={firstList} hasBigImages style="smallerMargin" />
-    <Divider />
-    <Article
-      title="Get helpful reminders so your plumbers never forget to clock in!"
-      description="When dealing with burst pipes and emergency call-outs, it’s easy to lose track of time. That’s why when your plumbers enter or leave a job site, and at their normal start and end time, we’ll remind them to clock in and out. And if they happen to forget, they can add a manual time entry for you to review later."
-      image={image1}
-      maxWidth={500}
-      imagePadding="7.3rem 4.5rem"
-      imageWidth={400}
-      imageHeight={354}
-    />
-    <Article
-      title="Entrust managers to track your cleaners’ time"
-      description="Dealing with commercial or industrial cleaning jobs that require lots of cleaners? Instead of having each cleaner track their time individually, your on-the-job manager can clock in and out on your cleaner’s behalf."
-      image={image1}
-      isSwapped
-      maxWidth={500}
-      imagePadding="7.3rem 4.5rem"
-      imageWidth={400}
-      imageHeight={354}
-    />
-    <SubscribeBanner
-      title="Let’s make it a clean sweep! Keep your time tracking tidy with Atto"
-      placeholder="Type your email"
-      bannerImage
-      checkItemOne="No credit card required"
-      checkItemTwo="14 day free trial"
-      checkItemThree="Cancel anytime"
-    />
-    <Footer FooterLinks={FooterLinks} />
-  </div>
+        author="Robert Bennett - DPA Cleaning Services, Inc."
+      />
+      <Divider />
+      <Title
+        title="Keep track of everything happening throughout your cleaners’ workday"
+        maxWidth={920}
+        maxDescriptionWidth={800}
+        description="Atto’s time tracking app is incredibly easy to use and allows you to see what all your cleaners are up to in real-time."
+      />
+      <Divider />
+      <IconCardList cardList={firstList} hasBigImages style="smallerMargin" />
+      <Divider />
+      <Article
+        title="Get helpful reminders so your plumbers never forget to clock in!"
+        description="When dealing with burst pipes and emergency call-outs, it’s easy to lose track of time. That’s why when your plumbers enter or leave a job site, and at their normal start and end time, we’ll remind them to clock in and out. And if they happen to forget, they can add a manual time entry for you to review later."
+        image={image1}
+        maxWidth={500}
+        imagePadding="7.3rem 4.5rem"
+        imageWidth={400}
+        imageHeight={354}
+      />
+      <Article
+        title="Entrust managers to track your cleaners’ time"
+        description="Dealing with commercial or industrial cleaning jobs that require lots of cleaners? Instead of having each cleaner track their time individually, your on-the-job manager can clock in and out on your cleaner’s behalf."
+        image={image1}
+        isSwapped
+        maxWidth={500}
+        imagePadding="7.3rem 4.5rem"
+        imageWidth={400}
+        imageHeight={354}
+      />
+      <Divider className="style3" />
+      <AccordionArticle
+        title="Keep track of your cleaners as they make their way to every client throughout the day"
+        list={accordionList1}
+      />
+      <Divider />
+      <AccordionArticle
+        title="Everything you need to keep cleaning jobs on schedule and on budget"
+        list={accordionList2}
+        isSwapped
+      />
+      <Divider className="style11" />
+      <Title
+        title="Say goodbye to paper timesheets!"
+        description="Ditch paper timesheets and switch to Atto – an easier and more accurate time-tracking solution for roofing companies"
+        maxWidth={1040}
+        maxDescriptionWidth={900}
+      />
+      <Divider className="style5" />
+      <OldVsNew oldList={oldList} newList={newList} />
+      <Divider />
+      <StaticImage src="../../../images/paper_timesheets@2x.png" />
+      <Divider className="style1" />
+      <Steps list={steps} />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <Divider />
+      <SubscribeBanner
+        title="Let’s make it a clean sweep! Keep your time tracking tidy with Atto"
+        placeholder="Type your email"
+        checkItemOne="No credit card required"
+        checkItemTwo="14 day free trial"
+        checkItemThree="Cancel anytime"
+      />
+      <Footer FooterLinks={FooterLinks} />
+    </div>
+  </>
 );
 
 export default Cleaning;
