9aac79141c9444084ff0689b3962c8cd7282911d
arbiasgjoshi <arbiasgjoshi@gmail.com>
2021-07-02 02:18:58 +0200

    submit form

---
+++ src/helpers/api.js
@@ -0,0 +1,15 @@
+// class Api {
+//   constructor() {}
+
+//   async signUp(data) {
+//     const response = await fetch(`https://attoime.com/api/v2/signup`, {
+//       method: 'POST',
+//       body: JSON.stringify(data),
+//     });
+
+//     const result = await response.json();
+//     return result;
+//   }
+// }
+
+// export default new Api();
